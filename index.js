d2efe2b045183dcbf2c31ad462c747577684207e
Shawn Presser <shawnpresser@gmail.com>
2019-06-26 08:58:47 -0500

    Improve the CLI

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ if (require.main === module) {
   if (process.argv.length >= 3) {
     process.nextTick(async () => {
       const baseUrl = 'file://' + __dirname + '/';
-      const u = new URL(process.argv[2], baseUrl).href;
-      const v = await sweetiekit.load(u, {sandbox: global, beforeRun({window, code, filename, sandbox, options}) {
-        sandbox.navigator = window.navigator
-        sandbox.document = window.document
-      }})
-      console.log(v.document)
+      const u = require('fs').existsSync(process.argv[2]) ? new URL(process.argv[2], baseUrl).href : new URL(process.argv[2]).href;
+      const v = await sweetiekit.load(u)
+      setTimeout(() => {
+        console.log(v.document)
+      }, 1000);
     })
   }
 }
